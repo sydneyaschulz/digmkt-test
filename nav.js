5b5639085552b0e8119ee447148e709c631635b0
sydneyaschulz <sydneyas@iastate.edu>
2026-09-08 13:01:50 -0500

    Refactor navigation element and update links

--- nav.js
+++ nav.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const nav = document.getElementById("navigation");
+    const navigation = document.getElementById("navigation");
 
-    nav.innerHTML = `
-        <nav class="navbar">
+    navigation.innerHTML = `
+        <nav class="top-nav">
 
-            <div class="nav-name">
-                <a href="index.html">Sydney Schulz</a>
-            </div>
+            <a href="#about">About Me</a>
 
-            <ul class="nav-links">
-                <li><a href="about.html">About Me</a></li>
-                <li><a href="jobs.html">Prior Jobs</a></li>
-                <li><a href="resume.html">Resume</a></li>
-                <li><a href="personal.html">Personal</a></li>
-            </ul>
+            <a href="#jobs">Prior Jobs</a>
+
+            <a href="#resume">Resume</a>
+
+            <a href="#personal">Personal</a>
 
         </nav>
     `;
+
 });
