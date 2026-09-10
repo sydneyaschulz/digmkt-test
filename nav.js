6446b37d931ff43753da674f34554349b0ecae02
sydneyaschulz <sydneyas@iastate.edu>
2026-09-10 13:18:53 -0500

    Refactor navigation HTML structure and links

--- nav.js
+++ nav.js
@@ -3,19 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const navigation = document.getElementById("navigation");
 
     navigation.innerHTML = `
-    <nav class="top-nav">
+        <nav class="top-nav">
 
-        <a href="#about">About Me</a>
+            <div class="nav-links">
 
-        <a href="#jobs">Prior Jobs</a>
+                <a href="index.html#about">About Me</a>
 
-        <a href="#resume">Resume</a>
+                <a href="index.html#jobs">Prior Jobs</a>
 
-        <a href="#personal">Personal</a>
+                <a href="index.html#resume">Resume</a>
 
-        <a href="#" class="products-link">Products</a>
+                <a href="index.html#personal">Personal</a>
 
-    </nav>
-`;
+                <a href="products.html" class="products-link">Products</a>
+
+            </div>
+
+        </nav>
+    `;
 
 });
