b26d5f63a192b7257e1c3a6ebeaf1eb416ada9db
sydneyaschulz <sydneyas@iastate.edu>
2026-09-10 13:16:29 -0500

    Add Products link to navigation

--- nav.js
+++ nav.js
@@ -3,17 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const navigation = document.getElementById("navigation");
 
     navigation.innerHTML = `
-        <nav class="top-nav">
+    <nav class="top-nav">
 
-            <a href="#about">About Me</a>
+        <a href="#about">About Me</a>
 
-            <a href="#jobs">Prior Jobs</a>
+        <a href="#jobs">Prior Jobs</a>
 
-            <a href="#resume">Resume</a>
+        <a href="#resume">Resume</a>
 
-            <a href="#personal">Personal</a>
+        <a href="#personal">Personal</a>
 
-        </nav>
-    `;
+        <a href="#" class="products-link">Products</a>
+
+    </nav>
+`;
 
 });
